7991b21f7ffa3a90c087aaab19fbd46660250728
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-08-23 15:45:30 -0300

    Method update Person

--- personaapi/src/main/java/com/digitalinnovation/personaapi/service/PersonService.java
+++ personaapi/src/main/java/com/digitalinnovation/personaapi/service/PersonService.java
@@ -3,6 +3,8 @@ package com.digitalinnovation.personaapi.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,9 +33,7 @@ public class PersonService {
 		Person personToSave = personMapper.toModel(personDTO);
 		
 		Person savedPerson = personRepository.save(personToSave);
-		return MessageResponseDTO.builder()
-				.message("Created person with ID" + savedPerson.getId())
-				.build();
+		return createMessageResponse(savedPerson.getId(), "Created person with ID");
 	}
 
 	public List<PersonDTO> listAll() {
@@ -53,8 +53,22 @@ public class PersonService {
 		personRepository.deleteById(id);
 	}
 
+	public MessageResponseDTO updateById(Long id, @Valid PersonDTO personDTO) throws PersonNotFoundException {
+		verifyIfExists(id);
+		
+		Person personToUpdate = personMapper.toModel(personDTO);
+		
+		Person updatedPerson = personRepository.save(personToUpdate);
+		return createMessageResponse(updatedPerson.getId(), "Updated person with ID");
+	}
+
 	private Person verifyIfExists(Long id) throws PersonNotFoundException {
 		return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
 	}
 	
+	private MessageResponseDTO createMessageResponse(Long id, String s) {
+		return MessageResponseDTO.builder()
+		.message(s + id)
+		.build();
+	}
 }
